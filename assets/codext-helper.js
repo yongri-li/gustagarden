3ec36d01eddd9a0e493a7688e24eda7378b7d38e
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2023-11-26 21:13:46 +0100

    U

---
+++ assets/codext-helper.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.href.includes('/products/')) {
+        const ratingsElement = document.querySelector('reviewsio-product-ratings');
+        const priceListContainer = document.querySelector('.product-meta__price-list-container');
+
+        if (ratingsElement && priceListContainer) {
+            priceListContainer.parentNode.insertBefore(ratingsElement, priceListContainer);
+
+            ratingsElement.style.marginTop = '-15px';
+            ratingsElement.style.display = 'block';
+        }
+    }
+});
